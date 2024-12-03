3ef44fde466a8f9770b69309617881b5a541db48
CursedScholares <cursedscholarlearning@gmail.com>
2024-12-02 20:53:24 -0500

    Update

--- README.md
+++ README.md
@@ -408,7 +408,7 @@ previous save data.
         }
     }
 ````
-# F - Add a “Buy Now” button to your product list. Your “Buy Now” bu�on must meet each of the following parameters:
+# F - Add a “Buy Now” button to your product list. Your “Buy Now” button must meet each of the following parameters:
 • The “Buy Now” button must be next to the buttons that update and delete products.
 
 • The button should decrement the inventory of that product by one. It should not affect the
--- src/main/java/com/example/demo/validators/EnufPartsValidator.java
+++ src/main/java/com/example/demo/validators/EnufPartsValidator.java
@@ -32,20 +32,23 @@ public class EnufPartsValidator implements ConstraintValidator<ValidEnufParts, P
         ProductService repo = myContext.getBean(ProductServiceImpl.class);
         if (product.getId() != 0) {
             Product myProduct = repo.findById((int) product.getId());
+            System.out.println(myProduct.getParts().size());
             for (Part p : myProduct.getParts()) {
-                if (p.getInv()<(product.getInv()-myProduct.getInv())) {
+                if (p.getMinInv() > (p.getInv() - (product.getInv()-myProduct.getInv()))){   //Work on this line
                     constraintValidatorContext.disableDefaultConstraintViolation();
                     constraintValidatorContext.buildConstraintViolationWithTemplate("This cannot be sanctioned as the input is smaller than the minimum allowed amount of units required.");
+                    return false;
                 }
                     if(p.getInv()>(p.getMaxInv())){
                         constraintValidatorContext.disableDefaultConstraintViolation();
                         constraintValidatorContext.buildConstraintViolationWithTemplate("This cannot be sanctioned as the input is greater than the maximum allowed amount of units required.");
+                        return false;
                     }
             }
             return true;
         }
         else{
-                return true;
+                return false;
             }
         }
     }
